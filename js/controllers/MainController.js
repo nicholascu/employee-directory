71142a20c63adba7014b0ebcbdc122db2ec08434
Nicholas Cu <nicholas.cu@pnmac.com>
2015-09-01 15:17:35 -0700

    Add orderBy sort

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -14,69 +14,9 @@ app.controller('MainController', ['$scope', function($scope) {
 			mail: "Mail stop: MP-212"
 		},
 		{
-			name: "Brandon Thomas",
-			title: "Appication Developer",
-			phone: "Office: (888) 555-5555" ,
-			address: "CA - Los Angeles 5898.",
-			manager: "Manager: Anand B",
-			email: "Penny Email: brandon.thomas@penny.com",
-			cost: "Cost center: 1001-006-0600",
-			mail: "Mail stop: MP-212"
-		},
-		{
-			name: "Brad Pitt",
-			title: "Appication Developer",
-			phone: "Office: (888) 555-5555" ,
-			address: "CA - Los Angeles 5898.",
-			manager: "Manager: Anand B",
-			email: "Penny Email: brad.pitt@penny.com",
-			cost: "Cost center: 1001-006-0600",
-			mail: "Mail stop: MP-212"
-		},
-		{
-			name: "Angelina Jolie",
-			title: "Appication Developer",
-			phone: "Office: (888) 555-5555" ,
-			address: "CA - Los Angeles 5898.",
-			manager: "Manager: Anand B",
-			email: "Penny Email: angelina.jolie@penny.com",
-			cost: "Cost center: 1001-006-0600",
-			mail: "Mail stop: MP-212"
-		},
-		{
-			name: "Karlie Kloss",
-			title: "Appication Developer",
-			phone: "Office: (888) 555-5555" ,
-			address: "CA - Los Angeles 5898.",
-			manager: "Manager: Anand B",
-			email: "Penny Email: karlie.kloss@penny.com",
-			cost: "Cost center: 1001-006-0600",
-			mail: "Mail stop: MP-212"
-		},
-		{
-			name: "Taylor Swift",
-			title: "Appication Developer",
-			phone: "Office: (888) 555-5555" ,
-			address: "CA - Los Angeles 5898.",
-			manager: "Manager: Anand B",
-			email: "Penny Email: taylor.swift@penny.com",
-			cost: "Cost center: 1001-006-0600",
-			mail: "Mail stop: MP-212"
-		},
-		{
-			name: "Alessandra Ambrosio",
-			title: "Appication Developer",
-			phone: "Office: (888) 555-5555" ,
-			address: "CA - Los Angeles 5898.",
-			manager: "Manager: Anand B",
-			email: "Penny Email: alessandra.ambrosio@penny.com",
-			cost: "Cost center: 1001-006-0600",
-			mail: "Mail stop: MP-212"
-		},
-		{
 			name: "Miranda Kerr",
 			title: "Appication Developer",
-			phone: "Office: (888) 555-5555" ,
+			phone: "Office: (888) 555-5550" ,
 			address: "CA - Los Angeles 5898.",
 			manager: "Manager: Anand B",
 			email: "Penny Email: miranda.kerr@penny.com",
@@ -86,10 +26,70 @@ app.controller('MainController', ['$scope', function($scope) {
 		{
 			name: "Minka Kelly",
 			title: "Appication Developer",
-			phone: "Office: (888) 555-5555" ,
+			phone: "Office: (888) 555-5551" ,
 			address: "CA - Los Angeles 5898.",
 			manager: "Manager: Anand B",
 			email: "Penny Email: minka.kelly@penny.com",
+			cost: "Cost center: 1001-006-0600",
+			mail: "Mail stop: MP-212"
+		},
+		{
+			name: "Karlie Kloss",
+			title: "Appication Developer",
+			phone: "Office: (888) 555-5552" ,
+			address: "CA - Los Angeles 5898.",
+			manager: "Manager: Anand B",
+			email: "Penny Email: karlie.kloss@penny.com",
+			cost: "Cost center: 1001-006-0600",
+			mail: "Mail stop: MP-212"
+		},
+		{
+			name: "Taylor Swift",
+			title: "Appication Developer",
+			phone: "Office: (888) 555-5553" ,
+			address: "CA - Los Angeles 5898.",
+			manager: "Manager: Anand B",
+			email: "Penny Email: taylor.swift@penny.com",
+			cost: "Cost center: 1001-006-0600",
+			mail: "Mail stop: MP-212"
+		},
+		{
+			name: "Alessandra Ambrosio",
+			title: "Appication Developer",
+			phone: "Office: (888) 555-5554" ,
+			address: "CA - Los Angeles 5898.",
+			manager: "Manager: Anand B",
+			email: "Penny Email: alessandra.ambrosio@penny.com",
+			cost: "Cost center: 1001-006-0600",
+			mail: "Mail stop: MP-212"
+		},
+		{
+			name: "Brandon Thomas",
+			title: "Appication Developer",
+			phone: "Office: (888) 555-5556" ,
+			address: "CA - Los Angeles 5898.",
+			manager: "Manager: Anand B",
+			email: "Penny Email: brandon.thomas@penny.com",
+			cost: "Cost center: 1001-006-0600",
+			mail: "Mail stop: MP-212"
+		},
+		{
+			name: "Brad Pitt",
+			title: "Appication Developer",
+			phone: "Office: (888) 555-5557" ,
+			address: "CA - Los Angeles 5898.",
+			manager: "Manager: Anand B",
+			email: "Penny Email: brad.pitt@penny.com",
+			cost: "Cost center: 1001-006-0600",
+			mail: "Mail stop: MP-212"
+		},
+		{
+			name: "Angelina Jolie",
+			title: "Appication Developer",
+			phone: "Office: (888) 555-5558" ,
+			address: "CA - Los Angeles 5898.",
+			manager: "Manager: Anand B",
+			email: "Penny Email: angelina.jolie@penny.com",
 			cost: "Cost center: 1001-006-0600",
 			mail: "Mail stop: MP-212"
 		}
